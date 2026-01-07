118fc0fe037783e7e33f5a666c8dd996994628bf
NirbhayaThapa <thapanirbhaya14@gmail.com>
2026-01-07 07:49:36 +0545

    Update db.php

--- db.php
+++ db.php
@@ -1,12 +1,13 @@
 <?php
 $host = 'localhost';
-$user = 'root';
-$pass = '';
-$dbname = 'student_portal';
+$user = 'np03cs4a240109';
+$pass = 'F6uxO6GbdI';
+$dbname = 'np03cs4a240109';
 
 $conn = new mysqli($host, $user, $pass, $dbname);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+
 ?>
